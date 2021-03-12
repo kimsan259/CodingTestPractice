1bfe2674f26569a978d179df9b43db4d65028df5
kimsan259 <79078395+kimsan259@users.noreply.github.com>
2021-03-13 02:17:56 +0900

    Add files via upload

---
+++ DP/2×n 타일링2 - 11727.cpp
@@ -0,0 +1,17 @@
+// 2*n 크기의 직사각형을 1*2, 2*1 2*2 타일로 채우는 방법의 수를 구하는 프로그램
+// 첫째줄에 n을 입력, 2*n 크기의 직사각형을 채우는 방법의 수를 10007로 나눈 나머지를 출력.
+// 2/3 8/171 12/2731
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int DP[1010] = {};
+	int n;
+	DP[0] = 1;  DP[1] = 1;
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i++) 
+		DP[i] = (DP[i - 1] + DP[i - 2]*2) % 10007;
+	printf("%d", DP[n] );
+	return 0;
+}
